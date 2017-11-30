9de5464063e60970b402bdc7b474564fbeafbc5e
Max Sergeev <sergeev.maxs@gmail.com>
2017-11-30 05:17:50 -0600

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,99 @@
+import { FsBannerService } from './fsbanner.service';
+import { FsBannerComponent } from './fsbanner.component';
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatStepperModule
+} from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+export * from './fsbanner.service';
+export * from './fsbanner.component';
+@NgModule({
+  imports: [
+    CommonModule,
+    HttpClientModule,
+
+    //MATERIAL
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    FlexLayoutModule
+    //MATERIAL END
+],
+declarations: [
+  FsBannerComponent
+],
+providers: [
+  FsBannerService
+],
+exports: [
+  FsBannerComponent
+]
+})
+export class FsBannerModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: FsBannerModule,
+      providers: [FsBannerService]
+    };
+  }
+}
